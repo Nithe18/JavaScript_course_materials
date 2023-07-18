6574e6f362b588badd894bd102ed216812332545
Nithe18 <119119933+Nithe18@users.noreply.github.com>
2023-07-18 15:35:38 +0100

    Add files via upload

---
+++ Starter/script.js
@@ -0,0 +1,17 @@
+let js = "amazing";
+console.log(10 + 20 + 30 - 30 * 5);
+
+console.log("Nitheesh");
+console.log(18);
+
+let firstName = "Nithe";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let nitheesh_nithe = "NN";
+let $new = 91;
+
+let person = "Nitheesh";
+let PI = 3.1415;
