51f252985e49ab9725ba9109e088d3f992ba6c42
Nithe18 <119119933+Nithe18@users.noreply.github.com>
2023-08-31 18:57:07 +0100

    Add files via upload

---
+++ datatypes/script.js
@@ -0,0 +1,45 @@
+/*
+let js = "amazing";
+//console.log(10 + 20 + 30 - 30 * 5);
+
+console.log("Nitheesh");
+console.log(18);
+
+let firstName = "Nithe";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// Variable name conventions
+let nitheesh_nithe = "NN";
+let $new = 91;
+
+let person = "Nitheesh";
+let PI = 3.1415;
+*/
+
+//Boolean
+
+let javascriptIsfun = true;
+console.log(javascriptIsfun);
+
+console.log(typeof true);
+console.log(javascriptIsfun);
+console.log(typeof 18);
+console.log(typeof 'Nitheesh');
+
+//Dynamic typing
+javascriptIsfun = 'YES!';
+console.log(typeof javascriptIsfun);
+
+//Undefined
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991
+console.log(typeof year);
+
+// the type of null is an error in Javascript
+console.log(typeof null);
