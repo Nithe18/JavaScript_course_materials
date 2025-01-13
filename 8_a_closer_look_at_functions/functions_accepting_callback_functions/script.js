be1d67086f2fac7f3edfdfae6e209151d573ade2
Nitheesh Vikraman <18nithe18@gmail.com>
2025-01-13 13:07:51 +0000

    Add files via upload

---
+++ 8_a_closer_look_at_functions/functions_accepting_callback_functions/script.js
@@ -0,0 +1,89 @@
+'use strict';
+/*
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1, // setting default parameters
+  price = 199 * numPassengers // expressions can be used while setting default parameters
+) {
+  // This is the old way of setting default parameters ES5
+  //numPassengers = numPassengers || 1;
+  //price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+createBooking('LH123', undefined, 1000);
+*/
+/*
+const flight = 'LH234';
+const nith = {
+  name: 'Nitheesh Vikraman',
+  passport: 1830570,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH288';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 1830570) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+//checkIn(flight, nith);
+//console.log(flight);
+//console.log(nith);
+
+// Is same as doing...
+//const flightNum = flight;
+//const passenger = nith;
+
+const newPasssport = function (person) {
+  person.passport = Math.trunc(Math.random() * 1000000);
+};
+
+newPasssport(nith);
+checkIn(flight, nith);
+*/
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function () {
+  console.log('👋🏼');
+};
+document.body.addEventListener('click', high5);
+['Nitheesh', 'Vikraman', 'Nith'].forEach(high5);
