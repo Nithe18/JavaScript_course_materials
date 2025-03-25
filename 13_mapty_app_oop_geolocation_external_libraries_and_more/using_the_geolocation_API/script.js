00eb3f10a52e409e393e0c41fbb6a5a5c9058b34
Nitheesh Vikraman <18nithe18@gmail.com>
2025-03-25 10:01:49 +0000

    Add files via upload

---
+++ 13_mapty_app_oop_geolocation_external_libraries_and_more/using_the_geolocation_API/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation)
+  // checking navigatior.geolocation exist in old browsers
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(
+        `https://www.google.com/maps/@${latitude},${longitude},14z?entry=ttu&g_ep=EgoyMDI1MDMxOS4yIKXMDSoASAFQAw%3D%3D`
+      );
+    },
+    function () {
+      alert('Could not get your position');
+    }
+  );
