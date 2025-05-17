3d6a81e193d5a8ed33baf058a9853049be2a1903
Nitheesh Vikraman <18nithe18@gmail.com>
2025-05-17 15:26:03 +0100

    Add files via upload

---
+++ 16_forkify_app_Building_a_modern_application_project/implementing_search_results_part_1/src/js/config.js
@@ -0,0 +1,2 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/'; // using upper case in the variable name because this is a constant that will never change
+export const TIMEOUT_SEC = 10;
---
+++ 16_forkify_app_Building_a_modern_application_project/implementing_search_results_part_1/src/js/controller.js
@@ -0,0 +1,45 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+
+import 'core-js/stable'; // polyfilling everything else
+import 'regenerator-runtime/runtime'; // polyfilling async/await
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+    // 1. Loading recipe
+    await model.loadRecipe(id); //as the loadRecipe function is a async function we have to await it
+
+    // 2. Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    console.log(err);
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    // 1. Get search qurey
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2. Load search results
+    await model.loadSearchResults(query);
+
+    // 3. Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+controlSearchResults();
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+};
+init();
