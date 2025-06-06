8cc3caaaaf4722fc55797d25454802da9ca061f4
Nitheesh Vikraman <18nithe18@gmail.com>
2025-06-06 15:48:41 +0100

    Delete 16_forkify_app_Building_a_modern_application_project/implementing_bookmarks_part_2 directory

--- 16_forkify_app_Building_a_modern_application_project/implementing_bookmarks_part_2/src/js/config.js
+++
@@ -1,3 +0,0 @@
-export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/'; // using upper case in the variable name because this is a constant that will never change
-export const TIMEOUT_SEC = 10;
-export const RES_PER_PAGE = 10;
