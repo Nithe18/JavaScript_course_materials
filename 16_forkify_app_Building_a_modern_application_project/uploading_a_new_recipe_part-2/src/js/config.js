68329a35303105c4674e0058c2959026b1bc9d56
Nitheesh Vikraman <18nithe18@gmail.com>
2025-06-12 01:07:26 +0100

    Add files via upload

---
+++ 16_forkify_app_Building_a_modern_application_project/uploading_a_new_recipe_part-2/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/'; // using upper case in the variable name because this is a constant that will never change
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'e7304a4e-7008-495f-a6f6-bc73b2ebb827';
+export const MODAL_CLOSE_SEC = 2.5;
